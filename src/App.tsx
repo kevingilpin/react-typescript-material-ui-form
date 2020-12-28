20e91737bd7a3fd96921125ef43f5523dfe6972b
Kevin Gilpin <kevin.gilpin@gmail.com>
2020-12-27 21:23:54 -0800

    Add lastname and email fields

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { MyForm } from './MyForm';
 
 const App = () => {
-  return <div style={{ textAlign: "center" }}><MyForm onSubmit={() => {}} /></div>
-}
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <MyForm onSubmit={() => {}} />
+    </div>
+  );
+};
 
 export default App;
--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -1,35 +1,51 @@
 import * as React from 'react';
-import { TextField } from "@material-ui/core";
-import {Formik, Form} from 'formik';
+import { TextField } from '@material-ui/core';
+import { Formik, Form } from 'formik';
 
 interface Values {
-    firstName: string;
-    lastName: string;
-    email: string;
+  firstName: string;
+  lastName: string;
+  email: string;
 }
 
 interface Props {
-    onSubmit: (values: Values) => void;
+  onSubmit: (values: Values) => void;
 }
 
-export const MyForm: React.FC<Props> = ({onSubmit}) => {
-    return (
-        <Formik initialValues={{firstName: '', lastName: '', email: ''}} onSubmit={(values) => {
-            onSubmit(values)
-        }}>
-            {({values, handleChange, handleBlur}) => (
-                <Form>
-                    <TextField 
-                        name='firstName' 
-                        value={values.firstName} 
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    <pre>
-                        {JSON.stringify(values, null, 2)}
-                    </pre>
-                </Form>
-            )}
-        </Formik>
-    );
-}
+export const MyForm: React.FC<Props> = ({ onSubmit }) => {
+  return (
+    <Formik
+      initialValues={{ firstName: '', lastName: '', email: '' }}
+      onSubmit={(values) => {
+        onSubmit(values);
+      }}
+    >
+      {({ values, handleChange, handleBlur }) => (
+        <Form>
+          <TextField
+            placeholder="first name"
+            name="firstName"
+            value={values.firstName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <TextField
+            placeholder="last name"
+            name="lastName"
+            value={values.lastName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <TextField
+            placeholder="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+        </Form>
+      )}
+    </Formik>
+  );
+};
