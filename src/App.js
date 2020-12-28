5cdf0cd31257311c52a43f7cb59561838c242a58
Kevin Gilpin <kevin.gilpin@gmail.com>
2020-12-27 21:12:39 -0800

    Update to TypeScript and display first field

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import logo from './logo.svg';
-import './App.css';
-
-const App = () => {
-  return <div style={{ textAlign: "center" }}>hello</div>
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MyForm } from './MyForm';
+
+const App = () => {
+  return <div style={{ textAlign: "center" }}><MyForm onSubmit={() => {}} /></div>
+}
+
+export default App;
